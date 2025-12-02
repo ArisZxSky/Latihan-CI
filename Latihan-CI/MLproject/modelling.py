c1ed03d8f1cd841df6f19be0b58e920b4726ad50
ArisZxSky <148959661+ArisZxSky@users.noreply.github.com>
2025-12-02 20:51:50 +0700

    Update modelling.py

--- Latihan-CI/MLproject/modelling.py
+++ Latihan-CI/MLproject/modelling.py
@@ -2,55 +2,41 @@ import mlflow
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
+import os
 import numpy as np
+import warnings
+import sys
 
-# Tidak perlu set_tracking_uri di CI/CD
-mlflow.set_experiment("Latihan Credit Scoring")
+if __name__ == "__main__":
+    warnings.filterwarnings("ignore")
+    np.random.seed(40)
 
-# Enable auto logging
-mlflow.autolog()
+    # Read the wine-quality csv file (make sure you're running this from the root of MLflow!)
+    file_path = sys.argv[3] if len(sys.argv) > 3 else os.path.join(os.path.dirname(os.path.abspath(__file__)), "train_pca.csv")
+    data = pd.read_csv(file_path)
 
-# Load dataset
-data = pd.read_csv("train_pca.csv")
-
-# Split data
-X_train, X_test, y_train, y_test = train_test_split(
+    # The predicted column is "quality" which is a scalar from [3, 9]
+    X_train, X_test, y_train, y_test = train_test_split(
     data.drop("Credit_Score", axis=1),
     data["Credit_Score"],
     random_state=42,
     test_size=0.2
-)
+    )
+    input_example = X_train[0:5]
+    n_estimators = int(sys.argv[1]) if len(sys.argv) > 1 else 505
+    max_depth = int(sys.argv[2]) if len(sys.argv) > 2 else 37
+    with mlflow.start_run():
+        model = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth)
+        model.fit(X_train, y_train)
 
-# Example for model signature
-input_example = X_train[0:5]
+        predicted_qualities = model.predict(X_test)
 
-# Parameter model
-n_estimators = 505
-max_depth = 37
-
-# Log params (optional, auto-log also logs them)
-mlflow.log_param("n_estimators", n_estimators)
-mlflow.log_param("max_depth", max_depth)
-
-# Train model
-model = RandomForestClassifier(
-    n_estimators=n_estimators,
-    max_depth=max_depth
-)
-
-model.fit(X_train, y_train)
-
-# Evaluate
-accuracy = model.score(X_test, y_test)
-
-# Log metric
-mlflow.log_metric("accuracy", accuracy)
-
-# Log model explicitly with input example
-mlflow.sklearn.log_model(
-    sk_model=model,
-    artifact_path="model",
-    input_example=input_example
-)
-
-print("Training completed successfully with accuracy:", accuracy)
+        mlflow.sklearn.log_model(
+        sk_model=model,
+        artifact_path="model",
+        input_example=input_example
+        )
+        model.fit(X_train, y_train)
+        # Log metrics
+        accuracy = model.score(X_test, y_test)
+        mlflow.log_metric("accuracy", accuracy)
